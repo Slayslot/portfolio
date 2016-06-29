8ce7bd4ac3b8211778463bf892acce77335c5894
Nikhil Handa <nikhilhanda@live.com>
2016-06-30 02:53:26 +0530

    Added Contact page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,13 @@ var app = angular.module('myPortfolio', ['ngRoute']);
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/intro', {
+      controller: "home-controller",
       templateUrl: 'views/home.html'
     })
-    // .when('/events', {
-    //   controller: "EventsController",
-    //   templateUrl: 'assets/views/events.html'
-    // })
+    .when('/contact', {
+      //controller: "ContactController",
+      templateUrl: 'views/contact.html'
+    })
 		// .when('/gallery', {
     //   controller: "GalleryController",
 		// 	templateUrl: 'assets/views/gallery.html'
@@ -44,3 +45,77 @@ $.fn.extend({
     });
   }
 });
+//
+// // $.Velocity.mock = 2;
+//
+// var menuOpen = [{
+//   elements: $('.nav'),
+//   properties: {
+//     height: ['100%', 0],
+//     width: ['100%', 0],
+//     minHeight: '40rem'
+//   },
+//   options: {
+//     duration: 200
+//   }
+// }, {
+//   elements: $('.right-inner'),
+//   properties: {
+//     width: '100%'
+//   },
+//   options: {
+//     duration: 250,
+//     complete: function() {
+//       $('.right-inner')
+//         .find('li').each(function(i) {
+//           $(this).velocity({
+//             opacity: 1
+//           }, {
+//             delay: i * 50
+//           });
+//         })
+//     }
+//   }
+// }];
+//
+// $(document.body).on('click', '.hamb',
+// function() {
+//
+//   if ($(this).hasClass('open')) {
+//     $(this)
+//       .removeClass('open')
+//       .find('i')
+//       .addClass('glyphicon-menu-hamburger')
+//       .removeClass('glyphicon-remove');
+//
+//     $('nav').velocity({
+//       height: 0,
+//       width: 0,
+//       minHeight: 0
+//     }, {
+//       duration: 250,
+//       begin: function() {
+//         $('nav').find('h1, p, small').css({
+//           opacity: 0
+//         })
+//         $('.right-inner').find('li, .social i').css({
+//           opacity: 0
+//         });
+//       },
+//       complete: function() {
+//         $('.right-inner').css({
+//           width: 0
+//         });
+//       }
+//     });
+//
+//   } else {
+//     $(this)
+//       .addClass('open')
+//       .find('i')
+//       .addClass('glyphicon-remove')
+//       .removeClass('glyphicon-menu-hamburger');
+//
+//     $.Velocity.RunSequence(menuOpen)
+//   }
+// });
