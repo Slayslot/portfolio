7a84c308d114624bc28893925ee9e927ac8429e1
Nikhil Handa <nikhilhanda@live.com>
2016-08-09 00:12:50 +0530

    Hides the page it's on from nav

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,12 +9,16 @@ var thisisme = [
 ]
 var thisismeiterator=0;
 
-function resetnav(){
+function resetnav(hidethisfuck){
+  $('.nav li').css('display','initial');
   $('.hamb').removeClass('open');
   $('.hamb i').addClass('icon-menu')
   $('.hamb i').removeClass('icon-cancel');
   $('.hamb').show();
   $('.nav').hide();
+  if(hidethisfuck!=null){
+    $('.'+hidethisfuck).css('display','none');
+  }
   var Arr=[];
   var Arrayiterator=0;
 }
@@ -35,14 +39,14 @@ app.controller('home-controller', ['$scope', function($scope) {
   },3000);
 }]);
 app.controller('work-controller', ['$scope','$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
-  resetnav();
+  resetnav('work');
   $('.work-image').hover(function(){
     $(this).find('i').animateCss('wobble');
   });
 }]);
 app.controller('about-controller', ['$scope','$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
-  resetnav();
+  resetnav('about');
 }]);
 app.controller('contact-controller', [function() {
-  resetnav();
+  resetnav('contact');
 }]);
