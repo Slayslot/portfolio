21bce99add77a8db179c43271d9dfbc4d83e660a
Nikhil Handa <nikhilhanda@live.com>
2016-08-09 20:27:45 +0530

    Added dot indicators

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -19,10 +19,83 @@ function resetnav(hidethisfuck){
   if(hidethisfuck!=null){
     $('.'+hidethisfuck).css('display','none');
   }
-  var Arr=[];
-  var Arrayiterator=0;
 }
+function isScrolledIntoView(el, param) {
+  var hT = $('#'+el).offset().top,
+      hH = $('#'+el).outerHeight(),
+      wH = $(window).height(),
+      wS = $(param).scrollTop();
+  if (wS+1 > (hT+hH-wH)){
+      return [{'tf': true,'elem': el}];
+  }
+}
+function scrollscroll(){
+  $(window).scroll(function(){
+    var bool=[];
+    var param=this;
+    var active;
+    $('.full').each(function(i, obj) {
+    bool[i]= isScrolledIntoView(obj.id,param);
+    if(bool[i]!=null){
+      if(bool[i][0].tf==true){
+        active=i;
+      }
+    }
+    });
+    var checkActive = $('#'+bool[active][0].elem+'-click').hasClass('current');
+    if(!checkActive){
+      $('#'+bool[active][0].elem+'-click').click();
+    }
+  });
+}
+;( function( window ) {
 
+  'use strict';
+
+  function extend( a, b ) {
+    for( var key in b ) {
+      if( b.hasOwnProperty( key ) ) {
+        a[key] = b[key];
+      }
+    }
+    return a;
+  }
+
+  function DotNav( el, options ) {
+    this.nav = el;
+    this.options = extend( {}, this.options );
+      extend( this.options, options );
+      this._init();
+  }
+
+  DotNav.prototype.options = {};
+
+  DotNav.prototype._init = function() {
+    // special case "dotstyle-hop"
+    var hop = this.nav.parentNode.className.indexOf( 'dotstyle-hop' ) !== -1;
+    var dots = [].slice.call( this.nav.querySelectorAll( 'li' ) ), current = 0, self = this;
+    dots.forEach( function( dot, idx ) {
+      dot.addEventListener( 'click', function( ev ) {
+        ev.preventDefault();
+        if( idx !== current ) {
+          dots[ current ].className = '';
+
+          setTimeout( function() {
+            dot.className += ' current';
+            current = idx;
+            if( typeof self.options.callback === 'function' ) {
+              self.options.callback( current );
+            }
+          }, 25 );
+        }
+      } );
+    } );
+  }
+
+  // add to global namespace
+  window.DotNav = DotNav;
+
+})( window );
 app.controller('home-controller', ['$scope', function($scope) {
   $('.hamb').hide();
   $('.nav').hide();
@@ -39,13 +112,29 @@ app.controller('home-controller', ['$scope', function($scope) {
   },3000);
 }]);
 app.controller('work-controller', ['$scope','$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
+  scrollscroll();
   resetnav('work');
   $('.work-image').hover(function(){
     $(this).find('i').animateCss('wobble');
   });
+  [].slice.call( document.querySelectorAll( '.dotstyle > ul' ) ).forEach( function( nav ) {
+  new DotNav( nav, {
+    callback : function( idx ) {
+      //console.log( idx );
+    }
+  } );
+} );
 }]);
 app.controller('about-controller', ['$scope','$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
+  scrollscroll();
   resetnav('about');
+  [].slice.call( document.querySelectorAll( '.dotstyle > ul' ) ).forEach( function( nav ) {
+  new DotNav( nav, {
+    callback : function( idx ) {
+      //console.log( idx );
+    }
+  } );
+} );
 }]);
 app.controller('contact-controller', [function() {
   resetnav('contact');
